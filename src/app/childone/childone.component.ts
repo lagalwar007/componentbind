883a45e0c00b24ba049023b4b2ff7eb9f61d53e6
Karan Lagalwar <lagalwar007@gmail.com>
2018-11-27 23:53:23 +0530

    Changes done in parentComponent

--- src/app/childone/childone.component.ts
+++ src/app/childone/childone.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   templateUrl: './childone.component.html',
   styleUrls: ['./childone.component.css']
 })
-export class ChildoneComponent{
-  public toggle:boolean = false;
+export class ChildoneComponent implements OnInit{
+  public toggle:boolean = true;
   @Output() toggleEvent = new EventEmitter<boolean>();
   constructor() { }
+  
+  ngOnInit(){
+   this.toggleEvent.emit(this.toggle); 
+  }
 
   getToggle(){
     this.toggle = !this.toggle;
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { ChildtwoComponent } from './../childtwo/childtwo.component';
+import { ChildoneComponent } from './../childone/childone.component';
+import {  Component, 
+          OnInit, 
+          AfterViewInit, 
+          AfterContentInit,
+          ViewChild, 
+          OnDestroy, 
+          AfterContentChecked } from '@angular/core';
+
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
-export class ParentComponent{
-
+export class ParentComponent implements OnInit,AfterViewInit,AfterContentInit,AfterContentChecked,OnDestroy{
   constructor() { }
   public toggle:boolean;
-
+  @ViewChild(ChildoneComponent) checkbox: ChildoneComponent;
+  @ViewChild(ChildtwoComponent) div: ChildtwoComponent;
+  ngOnInit(){
+    console.log('initialized')
+  }
+  ngAfterContentInit(){
+    console.log('afterContentInit');
+  }
+  ngAfterContentChecked(){
+    console.log('aftercontentChecked');
+  }
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(){
+    console.log('afterViewChecked');
+  }
+  
+  ngOnDestroy(){
+    console.log('onDestroy');
+  }
   setToggle($event){
     console.log('parent ===>',$event);
     this.toggle = $event;
